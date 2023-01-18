a81ce75d10709ea59344352fe11065b98c2a37f8
MRreddi <122854165+MRreddi@users.noreply.github.com>
2023-01-18 13:50:11 +0530

    Add files via upload

---
+++ Artihmeticcalculator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+a=int(input())
+b=int(input())
+print("Addition=")
+print(a+b)
+print("Substraction=")
+print(a-b)
+print("Multipication=")
+print(a*b)
+print("Division=")
+print(a/b)
+print("Remainder=")
+print(a%b)
+
+
